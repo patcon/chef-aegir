f4545f798eaf67ad0e365052fc53ef915a86d93f
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-05-12 12:28:54 -0400

    Initial commit with GPLv3 license.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Patrick Connolly (Myplanet Digital)"
+maintainer_email "patrick@myplanetdigital.com"
+license          "GNU Public License 3.0"
+description      "Installs/Configures chef-aegir"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
